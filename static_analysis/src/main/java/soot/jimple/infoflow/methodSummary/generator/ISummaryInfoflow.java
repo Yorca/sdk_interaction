3070deebbb27f4784aba7d2bfd16879bff93a713
privruler <privruler@gmail.com>
2024-08-05 06:41:48 -0400

    add control flow

--- static_analysis/src/main/java/soot/jimple/infoflow/methodSummary/generator/ISummaryInfoflow.java
+++
@@ -1,8 +0,0 @@
-package soot.jimple.infoflow.methodSummary.generator;
-
-import soot.jimple.infoflow.IInfoflow;
-import soot.jimple.infoflow.InfoflowManager;
-
-public interface ISummaryInfoflow extends IInfoflow {
-    InfoflowManager getManager();
-}
