5c1adb40536ff41bbca070f3864324aff8949af7
Zhaojie Hu <Yorcadm@yeah.net>
2024-09-03 16:42:43 -0400

    extract privacy docs

---
+++ py/implementation/Doc/web_archive/extrac_privacy_docs.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+
+def copy_privacy_files(source_directory):
+    destination_directory = 'privacy_docs'
+
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
+
+    for filename in os.listdir(source_directory):
+        if "privacy" in filename:
+            file_path = os.path.join(source_directory, filename)
+            if os.path.isfile(file_path):
+                shutil.copy(file_path, destination_directory)
+                print(f"Copied: {filename} to {destination_directory}")
+
+copy_privacy_files("website")
+copy_privacy_files("website_sup")
