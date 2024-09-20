1548c651672bed766801b8aaff09fea8c15f62ed
Zhaojie Hu <Yorcadm@yeah.net>
2024-09-20 15:54:07 -0400

    update download script

--- py/apk_downloader/timed_task.sh
+++ py/apk_downloader/timed_task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Terminate the previous running script if any
-pkill -f "/home/zh844971/sdk_interaction/sdk_interaction/py/apk_downloader/main.py"
+pkill -f "/home/zh844971/sdk_interaction/sdk_interaction/py/apk_downloader/downloader_v2.py"
 
 # Run the script
-python3 /home/zh844971/sdk_interaction/sdk_interaction/py/apk_downloader/main.py &
+python3 /home/zh844971/sdk_interaction/sdk_interaction/py/apk_downloader/downloader_v2.py &
