503633a879229fffcbc4c4b3156df42fd8e8aa94
Zhaojie Hu <Yorcadm@yeah.net>
2024-09-21 01:44:12 -0400

    simplifer

--- py/implementation/Doc/GPT Summarizer/simplifer.py
+++ py/implementation/Doc/GPT Summarizer/simplifer.py
@@ -41,23 +41,33 @@ def filter_similar_sentences(sentences):
     return kept_sentences
 
 summary_dir = "summary/merged"
-filter_dir = "summary/filtered"
+filter_dir = "summary/filtered2"
+original_count = 0
+new_count = 0
 for file_name in os.listdir(summary_dir):
     file_path = os.path.join(summary_dir, file_name)
+    print(file_path)
     with open(file_path, 'r') as file:
         data = file.read()
     js_data = json.loads(data)
     for api in js_data["privacy_APIs"]:
         print(api["effects"])
+        original_count += len(api["conditions"]) + len(api["effects"])
         api["conditions"] = filter_similar_sentences(api["conditions"])
         api["effects"] = filter_similar_sentences(api["effects"])
+        new_count += len(api["conditions"]) + len(api["effects"])
         for config in api["parameter_configurations"]:
+            original_count += len(config["conditions"]) + len(config["effects"])
             config["conditions"] = filter_similar_sentences(config["conditions"])
             config["effects"] = filter_similar_sentences(config["effects"])
+            new_count += len(config["conditions"]) + len(config["effects"])
+
 
     new_data = json.dumps(js_data, indent=4)
     with open(os.path.join(filter_dir, file_name), "w") as file:
         file.write(new_data)
+
+print(f"remove : {original_count-new_count} / {original_count}")
 
 # print(filter_similar_sentences([
 #                         "User has provided consent for GDPR.",
