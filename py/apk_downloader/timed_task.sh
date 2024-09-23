1411afa4eac4653cf4c5e9a72717eeeb01eef1b4
Zhaojie Hu <Yorcadm@yeah.net>
2024-09-22 21:16:07 -0400

    fix time task

--- py/apk_downloader/timed_task.sh
+++ py/apk_downloader/timed_task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Terminate the previous running script if any
-pkill -f "/home/zh844971/sdk_interaction/sdk_interaction/py/apk_downloader/downloader_v2.py"
+pkill -f "/Users/yorca/projects/sdk_interaction/py/apk_downloader/downloader_v2.py"
 
 # Run the script
-python3 /home/zh844971/sdk_interaction/sdk_interaction/py/apk_downloader/downloader_v2.py &
+/Users/yorca/projects/sdk_interaction/py/myenv/bin/python3.12 /Users/yorca/projects/sdk_interaction/py/apk_downloader/downloader_v2.py &
